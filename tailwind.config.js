c1c60b7021004dadc187d78ad4599c7654b967e6
Felipe Alessander <felipe03114@gmail.com>
2023-06-14 08:41:10 -0300

    ajustado tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,26 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: [
-      'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-      "./src/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-      extend: {
-            fontFamily: {
-                'Inter': ['Inter', 'sans-serif'],
-            },
-            screens: {
-                'sm-mobile': '280px', 
-                'md-mobile': '391px',
-                'lg-mobile': '415px',
-                'xl-mobile': '540px',
-                'tablet': '1050px'
-            },
-      },
-  },
-  plugins: [
-    require('flowbite/plugin'),
-    require('flowbite-typography'),
-],
+    content: [
+        'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+                fontFamily: {
+                    'Inter': ['Inter', 'sans-serif'],
+                },
+                screens: {
+                    'sm-mobile': '280px', 
+                    'md-mobile': '391px',
+                    'lg-mobile': '415px',
+                    'xl-mobile': '540px',
+                    'tablet': '1050px'
+                },
+        },
+    },
+    plugins: [
+        require('flowbite/plugin'),
+        require('flowbite-typography'),
+    ],
 }
